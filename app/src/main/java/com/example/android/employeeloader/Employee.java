4c661d32db655aadff84f6f270ba1884c8db46a3
Arvind <arvillu@gmail.com>
2017-12-11 14:14:52 +0530

    loader and asyntaskloader

---
+++ app/src/main/java/com/example/android/employeeloader/Employee.java
@@ -0,0 +1,15 @@
+package com.example.android.employeeloader;
+
+/**
+ * Created by Arvind on 11/12/17.
+ */
+
+public class Employee {
+    public String mEmpid;
+    public String mName;
+
+    public Employee(String id, String name) {
+        this.mEmpid = id;
+        this.mName = name;
+    }
+}
